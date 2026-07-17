42403aed54cea9a5297f3aa62bbe9c2ea4f610ff
prathamesh kshirsagar <prathameshkshirsagar532@gmail.com>
2026-07-17 16:56:47 +0530

    Implement twoSum function in ANS03.py

---
+++ QUESTIONS/0001_Array_Easy_Two_Sum/ANS03.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen={}
+        for i in range(len(nums)):
+            complement=target-nums[i]
+            if complement in seen:
+                return[i,seen[complement]]
+            seen[nums[i]]=i
+        return []
